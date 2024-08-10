579f26f6ebdf440f06479f36a6809cdbd46783e2
Bimo <bimosullivan10@gmail.com>
2024-08-10 17:29:17 +0700

    dropdown links component, change theme, and toggle theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import LinksDropdown from "./LinksDropdown";
-import ThemeToggle from "./ThemeToggle";
+
 import { UserButton } from "@clerk/nextjs";
+import ThemeToggle from "./ThemeToggle";
 
 export default function Navbar() {
   return (
     <nav className="bg-muted py-4 px-4 flex items-center justify-between sm:px-16 lg:px-24">
-      <LinksDropdown />
+      <div>
+        <LinksDropdown />
+      </div>
       <div className="flex items-center gap-x-4">
         <ThemeToggle />
         <UserButton afterSignOutUrl="/" />
