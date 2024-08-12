2336c5b4133c3db588f12a3bacbe5ea9f1d760bb
Bimo <bimosullivan10@gmail.com>
2024-08-12 13:52:02 +0700

    search form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,5 +1,66 @@
+"use client";
+
 import React from "react";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { JobStatus } from "@/utils/types";
+import { Button } from "./ui/button";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function SearchForm() {
-  return <div>SearchForm</div>;
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const search = formData.get("search") as string;
+    const jobStatus = formData.get("jobStatus") as string;
+
+    let params = new URLSearchParams();
+    params.set("search", search);
+    params.set("jobStatus", jobStatus);
+
+    router.push(`${pathname}?${params.toString()}`);
+
+    console.log({ search, jobStatus });
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="grid bg-muted mb-16 p-8 gap-4 rounded-lg sm:grid-cols-2 md:grid-cols-3"
+    >
+      <Input
+        type="text"
+        placeholder="Search Jobs"
+        name="search"
+        defaultValue={search}
+      />
+      <Select name="jobStatus" defaultValue={jobStatus}>
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+
+        <SelectContent>
+          {["all", ...Object.values(JobStatus)].map((jobStatus) => (
+            <SelectItem key={jobStatus} value={jobStatus}>
+              {jobStatus}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      <Button type="submit">Search</Button>
+    </form>
+  );
 }
